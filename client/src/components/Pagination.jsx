f83326270b693889ca56a14eb91fa4a20027e61d
Ifrahim Redwan <110650408+iredwan@users.noreply.github.com>
2025-06-25 11:53:49 +0600

    Make pagination controls responsive for smaller screens
    Adjusted the width and height of pagination buttons and break labels to use smaller sizes on small screens and default to larger sizes on medium screens and above. This improves usability and appearance on mobile devices.

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -9,17 +9,17 @@ export default function Pagination({ pageCount, onPageChange, currentPage = 0 })
       onPageChange={onPageChange}
       forcePage={currentPage}
       containerClassName="flex justify-center items-center gap-1 mt-12 select-none"
-      pageClassName="flex items-center justify-center w-10 h-10 rounded-full transition-all duration-200 bg-[#03588a] text-white hover:scale-105 cursor-pointer"
+      pageClassName="flex items-center justify-center w-8 h-8 md:w-10 md:h-10 rounded-full transition-all duration-200 bg-[#03588a] text-white hover:scale-105 cursor-pointer"
       pageLinkClassName="w-full h-full flex items-center justify-center"
       activeClassName="bg-primary text-white shadow-md"
       activeLinkClassName="font-medium"
       breakLabel={
-        <span className="w-10 h-10 flex items-center justify-center text-[#2878a8]">
+        <span className="md:w-8 md:h-8 w-1 h-1 flex items-center justify-center text-[#2878a8]">
           ...
         </span>
       }
       previousLabel={
-        <div className="w-10 h-10 flex items-center justify-center rounded-full bg-[#2878a8] hover:bg-[#023e61]  transition-colors">
+        <div className="w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full bg-[#2878a8] hover:bg-[#023e61]  transition-colors">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 text-primary"
@@ -37,7 +37,7 @@ export default function Pagination({ pageCount, onPageChange, currentPage = 0 })
         </div>
       }
       nextLabel={
-        <div className="w-10 h-10 flex items-center justify-center rounded-full bg-[#2878a8] hover:bg-[#023e61]  transition-colors">
+        <div className="w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full bg-[#2878a8] hover:bg-[#023e61]  transition-colors">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 text-primary"
